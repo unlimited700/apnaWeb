3ec6ad502f57e049b68e0d464f0bf53226949739
Anshul <malikanshul29@gmail.com>
2016-06-25 18:03:54 +0530

    git ooo

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config')
+var express = require('express');
+var request = require('request');
+var path = require('path');
 
-new WebpackDevServer(webpack(config), {
+var WebpackDevMiddleware = require('webpack-dev-middleware');
+var WebpackHotMiddlware = require('webpack-hot-middleware');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+var compiler = webpack(config);
+
+
+
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+app.use(WebpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    proxy: {
-        "/api/*": "http://localhost:3007/v1/problems"
-    }
-}).listen(8080, 'localhost', function(err, result) {
-   if(err) {
-       return console.log(err);
-   }
-    console.log("Listening at http://localhost:8080");
+    noInfo: false,
+    quite: false,
+    stats: {colors: true}
+}));
+
+app.use(WebpackHotMiddlware(compiler, {
+    log: console.log
+}))
+
+
+
+app.use('/api', function(req, res) {
+    console.log("request coming: " + req.url);
+    var url = "http://api.apnavaidya.com/" + req.url;
+    req.pipe(request(url)).pipe(res);
 });
+
+app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,11 +1,11 @@
 var Constants = {
     SEARCH: 'query',
     api: {
-        APP_HOME: 'http://localhost:3000/api',
+        APP_HOME: '/api',
         GET_PROBLEMS: '/v1/problems',
         PUBLIC_CREDENTIALS: {
             uid: 1,
-            authtoken: 'e8008f83-9f70-4df0-b7ea-676b22d9d335-malikanshul29@gmail.com-1466419597165'
+            authtoken: '5713b9d6-dbc6-415e-b8d1-db3be2961793-jpradeep.93@gmail.com-1460293796095'
         }
     },
 
